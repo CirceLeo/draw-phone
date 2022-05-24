b0213628d7d9ab229bab73e66c41f43343a027f8
Calypso Leonard <calypsorleonard@gmail.com>
2022-05-24 09:58:37 -0400

    started flipping images

--- client/src/components/RecentDrawingDisplay.js
+++ client/src/components/RecentDrawingDisplay.js
@@ -12,14 +12,24 @@ function RecentDrawingDisplay({displayPics}) {
 
     const renderedDrawings = displayPics.map((drawing) => {
         return (
-                <div key={drawing.id}  className="display-drawing"   >
-                    <img 
-                        src={drawing.data_url} 
-                        onMouseOver={e => (e.currentTarget.src = drawing.origin_pic_url)}
-                        onMouseOut={e => (e.currentTarget.src = drawing.data_url)}
-                    />
-                    <p>Artist: {drawing.user ? drawing.user.username: "anon"}</p>
-                    <p>Title: {drawing.title ? drawing.title :` untitled piece ${drawing.id}`}</p>
+                <div key={drawing.id}  className="display-drawing flip-card">
+                    <div className='flip-card-inner'>
+                        <div className='flip-card-front'>
+                            <img 
+                                src={drawing.data_url} 
+                                // onMouseOver={e => (e.currentTarget.src = drawing.origin_pic_url)}
+                                // onMouseOut={e => (e.currentTarget.src = drawing.data_url)}
+                            />
+                        </div>
+                        <div className='flip-card-back'>
+                            <img src={drawing.origin_pic_url}/>
+                            {/* TODO: change schema of drawings to include origin topics and time spent, then put here */}
+                        </div>
+                        <div className='display-drawing-text'>
+                            <p>Artist: {drawing.user ? drawing.user.username: "anon"}</p>
+                            <p>Title: {drawing.title ? drawing.title :` untitled piece ${drawing.id}`}</p>
+                        </div>
+                    </div>
                 </div>
             )
     })
