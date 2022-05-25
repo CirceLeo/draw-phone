de73a61ca7f52d293cec0df3faec072841e2d087
Calypso Leonard <calypsorleonard@gmail.com>
2022-05-25 15:44:02 -0400

    really messing with things now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,11 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
