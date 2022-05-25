d7f8bd758756ffd50aa77933d80759f13046e535
Calypso Leonard <calypsorleonard@gmail.com>
2022-05-25 15:27:27 -0400

    trying to deply

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "htt[://localhost:4000"
-        # origins "*"
+        # origins "http://localhost:4000"
+        origins "*"
 
         resource "*",
             headers: :any,
