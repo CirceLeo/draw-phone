921c5e84b400758b881d3d7d315f077592974c97
Calypso Leonard <calypsorleonard@gmail.com>
2022-05-28 18:02:32 -0400

    flip cards!

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './styling/welcome.css';
 import './styling/modal.css'
 import './styling/userAdmin.css'
 import './styling/Gallery.css'
+import './styling/drawingDisplay.css'
 
 
 import React, {useEffect, useState, useRef, useContext} from 'react';
--- client/src/components/Drawing and other User Displays/DisplayDrawing.js
+++ client/src/components/Drawing and other User Displays/DisplayDrawing.js
@@ -1,7 +1,33 @@
-function DisplayDrawing(props) {
-    //TODO: actually pull that stuff out here
+import {useState} from 'react'
+import CreateChallengeForm from '../Challenges/CreateChallengeForm'
+
+function DisplayDrawing({drawing, artistDetails, userPage, openAddFriend}) {
+
+    const [backPicLoaded, setBackPicLoaded] = useState(false)
+    const [challengeOpen, setChallengeOpen] = useState(false)
+
+    const contentLoading = <div className='display-pic-loading'><p>origin pic loading!</p></div>
+
     return (
-        <></>
+        <div key={drawing.id} className=" display-drawing flip-card">
+            <div className='flip-card-content'>
+                <div className='flip-card-front'>
+                    <img src={drawing.data_url} />
+                </div>
+                <div className='flip-card-back'>
+                    {backPicLoaded ? null : <>{contentLoading}</>}
+                    <img onLoad={() => setBackPicLoaded(true)} src={drawing.origin_pic_url}/> 
+                </div>
+            </div>
+            <div className='display-drawing-text'>
+                    {artistDetails ?  <p><strong>Artist: </strong> {drawing.user.username !== "guest"  ? <em className='clickable-username' onClick={() => openAddFriend(drawing.user.id)}>{drawing.user.username}</em>: "anon"}</p> : null}
+                    <p><strong>Title: </strong> <em> {drawing.title ? drawing.title :` untitled piece ${drawing.id}`}</em></p>
+                    <p><strong>Completed in:</strong> {drawing.play_time ? <em>{drawing.play_time} seconds</em> : "unknown" }  </p>
+                    {userPage? <button onClick={() => setChallengeOpen(true)}>create a challenge?</button> : null}
+                    {challengeOpen ? <CreateChallengeForm  drawingId={drawing.id} close={()=> setChallengeOpen(false)}/> : null}
+                    {/* var dataURI = canvas.toDataURL("image/jpeg", 0.2);  // type, enc. option <0.0, 1.0] */}
+            </div>
+        </div>
     )
 }
 export default DisplayDrawing
