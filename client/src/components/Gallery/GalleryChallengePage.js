a4dbf74e4c50983b0cc3a400fc5a08e16c8b8236
Calypso Leonard <calypsorleonard@gmail.com>
2022-05-31 15:12:09 -0400

    making the gallery nice

--- client/src/components/Gallery/GalleryChallengePage.js
+++ client/src/components/Gallery/GalleryChallengePage.js
@@ -2,6 +2,7 @@ import {useParams} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 import Header from '../Admin/Header'
 import Footer from '../Admin/Footer'
+import Gallery from './Gallery'
 
 function GalleryChallengePage(props) {
 
@@ -35,7 +36,7 @@ function GalleryChallengePage(props) {
             <h1>Challenge Attempts for <em>{challenge.challenge_title}</em></h1>
             <img src={challenge.drawing.origin_pic_url} />
             <img src={challenge.drawing.data_url} />
-            {renderedAttempts}
+            {challenge.attempts.length > 0 ? {renderedAttempts} : <p>There are no attempts for this challenge! Click <a href={`/play/challenge/${challenge.id}`} /> to play the challenge! </p>}
             
             <div className='challenge-display-page'>
 
