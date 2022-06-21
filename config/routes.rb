042257fe00ea2c11c8d90ef8f7d88f13bc9feac9
Calypso Leonard <calypsorleonard@gmail.com>
2022-06-21 14:41:11 -0400

    changed api to fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     !request.xhr? && request.format.html?
   end
 
-  root to: 'home#index'
+
 
   resources :challenges
   get '/challenges/trending', to: 'challenges#trending'
@@ -65,5 +65,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  root to: 'home#index'
 end
